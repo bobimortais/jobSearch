34ab8fe34d5ff00f0bede30a934d11e71b25f527
bobimortais <robsonzacarias@hotmail.com>
2019-12-06 01:33:37 -0300

    Added Moberries to the list of sites to search

--- searchJobs.py
+++ searchJobs.py
@@ -1,6 +1,12 @@
 import requests
 import json
 
+#URLs to Search
+linkedInURL = 'https://www.linkedin.com/jobs/search/?f_TPR=r604800&geoId=103644278&keywords=remote%20java%20developer&location=United%20States&originalSubdomain=br'
+landingJobsURL = 'https://landing.jobs/jobs/search?page=1&remote=true&hd=false&t_co=false&t_st=false'
+moberriesURL = 'https://www.moberries.com/jobs-search?q=remote&category=1'
+
+
 def printLinkedInJobs(jobs):
 	for index in range(1, len(jobs)):
 		jobTitle = jobs[index].split('job-result-card__title">')[1].split('<')[0]
@@ -24,6 +30,20 @@ def printLandingJobsJobs(jobs):
 		print('Job Link: ' + job['url'])
 		print('\n')
 		
+def printMoberriesJobs(jobs):
+	jobs = jobs.split('media">')
+	for index in range(1, len(jobs)):
+		jobTitle = jobs[index].split('<a')[1].split('>')[1].split('<')[0]
+		company = jobs[index].split('span>')[1].split('<')[0]
+		location = jobs[index].split('tag-list">')[1].split('</i>')[1].split('</span>')[0].split('>')[1]
+		jobLink = 'https://www.moberries.com/job/' + jobs[index].split('/job/')[1].split('"')[0]
+		print('Title: ' + jobTitle)
+		print('Company: ' + company)
+		print('Location: ' + location)
+		print('Job Link: ' + jobLink)
+		print('\n')
+	
+		
 def searchJobsFromURL(url):
 	URL = url
 	r = requests.get(url = URL)
@@ -31,13 +51,17 @@ def searchJobsFromURL(url):
 	return responseTOString
 
 #Linkedin
-responseTOString = searchJobsFromURL('https://www.linkedin.com/jobs/search/?f_TPR=r604800&geoId=103644278&keywords=remote%20java%20developer&location=United%20States&originalSubdomain=br')
+responseTOString = searchJobsFromURL(linkedInURL)
 jobs = responseTOString.split('result-card job-result-card')
 printLinkedInJobs(jobs)
 
 #LandingJobs
-responseTOString = searchJobsFromURL('https://landing.jobs/jobs/search?page=1&remote=true&hd=false&t_co=false&t_st=false')
+responseTOString = searchJobsFromURL(landingJobsURL)
 jsonReponse = json.loads(responseTOString)
 jobs = jsonReponse['offers']
 printLandingJobsJobs(jobs)
-	
+
+#Moberries
+responseTOString = searchJobsFromURL(moberriesURL)
+jobs = responseTOString.split('job-list')[1]
+printMoberriesJobs(jobs)
